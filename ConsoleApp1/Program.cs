20d7658b1bc713f2bd1f14c8ee07efa575ddbbd1
viktor <viktor.kononenko.2003@mail.ru>
2022-01-14 17:30:05 +0500

    msg

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("1");
-Console.WriteLine("2");
-Console.WriteLine("3");
+Console.WriteLine("Изmенения");
+
